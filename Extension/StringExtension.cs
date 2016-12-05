452245f0e68c364568995d64123334b00c8c001a
chn_yongsheng <1062612096@qq.com>
2016-12-05 16:18:44 +0800

    获取字符串的MD5码

---
+++ Extension/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class StringExtension
+    {
+        private static readonly MD5 _md5 = MD5.Create();
+        private static readonly SpinLock _spinLock = new SpinLock();
+        private static bool _lockTaken = false;
+        public static string GetMD5HashCode(this string str)
+        {
+            try
+            {
+                _spinLock.Enter(ref _lockTaken);
+                return string.Join("", _md5.ComputeHash(Encoding.UTF8.GetBytes(str)).Select(x => x.ToString("x2")));
+            }
+            finally
+            {
+                if (_lockTaken)
+                    _spinLock.Exit();
+            }
+        }
+    }
+}
